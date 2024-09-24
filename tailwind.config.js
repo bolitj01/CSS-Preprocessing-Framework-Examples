f9362bbd60cd0e685478a9e8e7eb7554036ede97
Thomas Bolinger <bolitj01@pfw.edu>
2024-09-24 12:01:42 -0400

    Better property order on exercise

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './tailwind/**/*.{html,js}', // Include all HTML, JS, and CSS files in the tailwind folder
+    './tailwind/**/*.{html,js}', // Include all HTML, JS files in the tailwind folder
   ],
   theme: {
     extend: {},
